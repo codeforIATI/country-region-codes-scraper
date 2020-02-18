0cce22e0e75d46685d68c4fb62a877613adec414
Mark Brough <mark.brough@gmail.com>
2020-02-18 12:53:19 +0100

    Add in 2-digit ISO code and add some debugging lines in case

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
 URL = "https://unstats.un.org/unsd/methodology/m49/overview/"
+COUNTRIES_MORPH_API_URL = "https://api.morph.io/andylolz/country-codes/data.json"
 
 from lxml import html
 from os import environ, remove
 environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
+
+morph_api_key = environ['MORPH_API_KEY']
 import scraperwiki
 import requests
 import shutil
@@ -25,16 +28,29 @@ LANGS = ["ZH", "RU", "FR", "ES", "AR"]
 LANG_COLS = ['Global Name', 'Region Name', 'Sub-region Name',
     'Intermediate Region Name', 'Country or Area']
 
+def get_countries_data():
+    print("Getting countries data...")
+    r = requests.get(COUNTRIES_MORPH_API_URL, params={
+      'key': morph_api_key,
+      'query': 'select * from "data"'
+    })
+    data = r.json()
+    return dict(map(lambda c: (c['code_3_digit'], c), data))
+
 def get_page():
     r = requests.get(URL)
     return html.fromstring(r.text)
 
 def run():
+    print("Starting up...")
+    countries_data = get_countries_data()
+    print("Got countries data.")
+    print("Getting UN M49 data.")
     page = get_page()
 
     bigdata = {}
-
     # Go through the EN table to get all basic data + EN data
+    print("Getting basic data and EN data.")
     table = page.xpath("//table[@id='downloadTableEN']")[0]
     for row in table.xpath("tbody/tr"):
         cols = row.xpath("td")
@@ -53,6 +69,7 @@ def run():
         bigdata[data['M49 Code']] = data
     # Parse the language-dependent columns again for each language
     for lang in LANGS:
+        print("Getting data for {} language".format(lang))
         table = page.xpath("//table[@id='downloadTable{}']".format(lang))[0]
         for row in table.xpath("tbody/tr"):
             cols = row.xpath("td")
@@ -60,7 +77,12 @@ def run():
             for lang_col in LANG_COLS:
                 bigdata[M49_CODE].update({'{}_{}'.format(lang_col, lang): cols[HEADERS.index(lang_col)].text })
     # Save everything to scraperwiki
+    print("Saving data to database...")
     for bd in bigdata.values():
+        country_data = countries_data.get(bd['ISO-alpha3 Code'])
+        if country_data:
+            bd['ISO-alpha2 Code'] = country_data['code']
         scraperwiki.sqlite.save(unique_keys=['M49 Code'], data=bd)
+    print("Done.")
 
 run()
